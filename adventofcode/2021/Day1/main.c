1f47df450f9e654893f0ac2e0b972a71a621df5f
Annika Hansson <annika.hansson@saffran.se>
2022-11-27 16:04:12 +0100

    Second part of day 1

--- adventofcode/2021/Day1/main.c
+++ adventofcode/2021/Day1/main.c
@@ -3,12 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main(int argc, char **argv){
-
-    if(argv[1] == NULL){
-        printf("Give a file as an argument.");
-        exit(0);
-    }
+void part_one(char * input){
     FILE * fp;
     char * line = NULL;
     size_t len = 0;
@@ -18,7 +13,7 @@ void main(int argc, char **argv){
     int current_value = 0;
     int decreased = 0;
 
-    fp = fopen(argv[1], "r");
+    fp = fopen(input, "r");
     while((read = getline(&line, &len, fp)) != -1){
         printf("Retrieved line of length %zu:\n", read);
         printf("%s\n", line);
@@ -46,6 +41,67 @@ void main(int argc, char **argv){
 
     printf("Increased: %d\n", increased);
     printf("Decreased: %d\n", decreased);
+}
+
+void part_two(char * input){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int second_value = 0;
+    int increased = 0;
+    int first_value = 0;
+    int decreased = 0;
+    int third_value = 0;
+    int sum1 = 0;
+    int sum2 = 0;
+
+    fp = fopen(input, "r");
+    while((read = getline(&line, &len, fp)) != -1){
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s\n", line);
+        first_value = atoi(line);
+        if(second_value != 0 && third_value != 0){
+            printf("F: %d\nS: %d\nT: %d\n", first_value, second_value, third_value);
+            sum1 = first_value + second_value + third_value;
+            if(sum2 != 0){
+                printf("S1: %d\nS2: %d\n", sum1, sum2);
+                if((sum1 - sum2) > 0){
+                    printf("Increased\n");
+                    increased += 1;
+                }
+                else if ((sum1 - sum2) < 0){
+                    printf("Decreased\n");
+                    decreased += 1;
+                }
+            }
+            sum2 = sum1;
+        }
+        third_value = second_value;
+        second_value = first_value;
+    }
+
+    fclose(fp);
+    if(line){
+        free(line);
+    }
+
+    printf("Increased: %d\n", increased);
+    printf("Decreased: %d\n", decreased);
+}
+
+void main(int argc, char **argv){
+
+    if(argv[1] == NULL){
+        printf("Give a file as an argument.");
+        exit(0);
+    }
+
+    printf("Part one:\n");
+    part_one(argv[1]);
+
+    printf("\n\nPart two:\n");
+    part_two(argv[1]);
 
     exit(0);
 }
